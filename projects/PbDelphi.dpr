4b07a15f41122c1b690a9c3b1a9314b29346b083
marat1961 <marat.sh.1961@gmail.com>
2020-08-02 22:43:49 +0700

    Added a template for a code generator project

---
+++ projects/PbDelphi.dpr
@@ -0,0 +1,24 @@
+program PbDelphi;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils,
+  Oz.Pb.Gen in '..\src\protoc\Oz.Pb.Gen.pas',
+  Oz.Pb.Options in '..\src\protoc\Oz.Pb.Options.pas',
+  Oz.Pb.Tab in '..\src\protoc\Oz.Pb.Tab.pas',
+  Oz.Pb.Parser in '..\src\protoc\Oz.Pb.Parser.pas',
+  Oz.Pb.Scanner in '..\src\protoc\Oz.Pb.Scanner.pas',
+  Oz.Cocor.Lib in '..\src\protoc\Oz.Cocor.Lib.pas',
+  Oz.Cocor.Utils in '..\src\protoc\Oz.Cocor.Utils.pas';
+
+begin
+  try
+    { TODO -oUser -cConsole Main : Insert code here }
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
