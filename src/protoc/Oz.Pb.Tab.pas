583efb23c4558ade3a796a27adf212592088d67f
marat1961 <marat.sh.1961@gmail.com>
2020-09-23 00:49:55 +0700

    Import from several files (modules) has been implemented. Passed a simple test.

--- src/protoc/Oz.Pb.Tab.pas
+++ src/protoc/Oz.Pb.Tab.pas
@@ -707,7 +707,12 @@ begin
     end;
     if s = FUniverse then
     begin
-      obj := x; parser.SemError(2);
+      x := FModule.FImport.dsc;
+      while x.name <> id do
+        x := x.next;
+      if x = FGuard then
+        parser.SemError(2);
+      obj := x;
       exit;
     end;
     s := s.dsc;
@@ -827,14 +832,24 @@ const
   // This is a predefined module describing embedded structures and types.
   PredefinedModule = 'google/protobuf/descriptor.proto';
 var
-  tm: TModule;
+  tm: TModule; tp: TBaseParser;
+  x: PObj;
 begin
   if LowerCase(id) = PredefinedModule then exit;
-  tm := FModule;
+  tm := FModule; tp := FParser;
   try
     OpenProto(id, Weak);
+    if tm.FImport = nil then
+      tm.FImport := FModule.Obj
+    else
+    begin
+      x := tm.FImport;
+      while x.next <> FGuard do
+        x := x.next;
+      x.next := FModule.Obj;
+    end;
   finally
-    FModule := tm;
+    FModule := tm; FParser := tp;
   end;
 end;
 
