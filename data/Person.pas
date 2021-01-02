8e152505f60d288f4c3ec63b8d0f64ce75eb94a8
marat1961 <marat.sh.1961@gmail.com>
2021-01-02 23:49:35 +0700

    Added a simple test using SGL collections.

---
+++ data/Person.pas
@@ -0,0 +1,216 @@
+unit Person;
+
+interface
+
+uses
+  System.Classes, System.SysUtils, Oz.SGL.Collections, Oz.Pb.Classes;
+
+{$T+}
+
+type
+
+  TPhoneType = (
+    MOBILE = 0,
+    HOME = 1,
+    WORK = 2);
+
+  PPhoneNumber = ^TPhoneNumber;
+  TPhoneNumber = record
+  const
+    ftNumber = 1;
+    ftType = 2;
+  private
+    FNumber: string;
+    FType: TPhoneType;
+  public
+    procedure Init;
+    procedure Free;
+    // properties
+    property Number: string read FNumber write FNumber;
+    property &Type: TPhoneType read FType write FType;
+  end;
+
+  PPerson = ^TPerson;
+  TPerson = record
+  const
+    ftName = 1;
+    ftId = 2;
+    ftEmail = 3;
+    ftPhones = 4;
+    ftMyPhone = 5;
+  private
+    FName: string;
+    FId: Integer;
+    FEmail: string;
+    FPhones: TsgRecordList<TPhoneNumber>;
+    FMyPhone: TPhoneNumber;
+  public
+    procedure Init;
+    procedure Free;
+    // properties
+    property Name: string read FName write FName;
+    property Id: Integer read FId write FId;
+    property Email: string read FEmail write FEmail;
+    property Phones: TsgRecordList<TPhoneNumber> read FPhones;
+    property MyPhone: TPhoneNumber read FMyPhone write FMyPhone;
+  end;
+
+  TLoadHelper = record helper for TpbLoader
+  public
+    procedure LoadPerson(Person: PPerson);
+    procedure LoadPhoneNumber(PhoneNumber: PPhoneNumber);
+  end;
+
+  TSaveHelper = record helper for TpbSaver
+  public
+    procedure SavePerson(Person: PPerson);
+    procedure SavePhoneNumber(PhoneNumber: PPhoneNumber);
+  end;
+
+implementation
+
+{ TPhoneNumber }
+
+procedure TPhoneNumber.Free;
+begin
+
+end;
+
+procedure TPhoneNumber.Init;
+begin
+
+end;
+
+{ TPerson }
+
+procedure TPerson.Init;
+begin
+  FPhones := TsgRecordList<TPhoneNumber>.From(nil);
+end;
+
+procedure TPerson.Free;
+begin
+  FPhones.Free;
+end;
+
+procedure TLoadHelper.LoadPhoneNumber(PhoneNumber: PPhoneNumber);
+var
+  fieldNumber, wireType: integer;
+  tag: TpbTag;
+begin
+  tag := Pb.readTag;
+  while tag.v <> 0 do
+  begin
+    wireType := tag.WireType;
+    fieldNumber := tag.FieldNumber;
+    case fieldNumber of
+      TPhoneNumber.ftNumber:
+        begin
+          Assert(wireType = TWire.LENGTH_DELIMITED);
+          PhoneNumber.Number := Pb.readString;
+        end;
+      TPhoneNumber.ftType:
+        begin
+          Assert(wireType = TWire.VARINT);
+          PhoneNumber.&Type := TPhoneType(Pb.readInt32);
+        end;
+      else
+        Pb.skipField(tag);
+    end;
+    tag := Pb.readTag;
+  end;
+end;
+
+procedure TLoadHelper.LoadPerson(Person: PPerson);
+var
+  fieldNumber, wireType: integer;
+  tag: TpbTag;
+  Phone: PPhoneNumber;
+begin
+  tag := Pb.readTag;
+  while tag.v <> 0 do
+  begin
+    wireType := tag.WireType;
+    fieldNumber := tag.FieldNumber;
+    case fieldNumber of
+      TPerson.ftName:
+        begin
+          Assert(wireType = TWire.LENGTH_DELIMITED);
+          Person.Name := Pb.readString;
+        end;
+      TPerson.ftId:
+        begin
+          Assert(wireType = TWire.VARINT);
+          Person.Id := Pb.readInt32;
+        end;
+      TPerson.ftEmail:
+        begin
+          Assert(wireType = TWire.LENGTH_DELIMITED);
+          Person.Email := Pb.readString;
+        end;
+      TPerson.ftPhones:
+        begin
+          Assert(wireType = TWire.LENGTH_DELIMITED);
+          Pb.Push;
+          try
+            Phone := Person.FPhones.Add;
+            LoadPhoneNumber(Phone);
+          finally
+            Pb.Pop;
+          end;
+        end;
+      TPerson.ftMyPhone:
+        begin
+          Assert(wireType = TWire.LENGTH_DELIMITED);
+          Pb.Push;
+          try
+            LoadPhoneNumber(@Person.FMyPhone);
+          finally
+            Pb.Pop;
+          end;
+        end;
+      else
+        Pb.skipField(tag);
+    end;
+    tag := Pb.readTag;
+  end;
+end;
+
+procedure TSaveHelper.SavePhoneNumber(PhoneNumber: PPhoneNumber);
+begin
+  Pb.writeString(TPhoneNumber.ftNumber, PhoneNumber.Number);
+  Pb.writeInt32(TPhoneNumber.ftType, Ord(PhoneNumber.&Type));
+end;
+
+procedure TSaveHelper.SavePerson(Person: PPerson);
+var
+  i: Integer;
+  h: TpbSaver;
+begin
+  Pb.writeString(TPerson.ftName, Person.Name);
+  Pb.writeInt32(TPerson.ftId, Person.Id);
+  Pb.writeString(TPerson.ftEmail, Person.Email);
+  if Person.FPhones.Count > 0 then
+  begin
+    h.Init;
+    try
+      for i := 0 to Person.FPhones.Count - 1 do
+      begin
+        h.Clear;
+        h.SavePhoneNumber(Person.Phones.Items[i]);
+        Pb.writeMessage(TPerson.ftPhones, h.Pb^);
+      end;
+    finally
+      h.Free;
+    end;
+  end;
+  h.Init;
+  try
+    h.SavePhoneNumber(@Person.MyPhone);
+    Pb.writeMessage(TPerson.ftMyPhone, h.Pb^);
+  finally
+    h.Free;
+  end;
+end;
+
+end.
---
+++ test/Project1.dpr
@@ -0,0 +1,123 @@
+﻿(* Protocol buffer code generator, for Delphi
+ * Copyright (c) 2020 Marat Shaimardanov
+ *
+ * This file is part of Protocol buffer code generator, for Delphi
+ * is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this file. If not, see <https://www.gnu.org/licenses/>.
+ *)
+
+program Project1;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+  TypInfo,
+  Oz.Pb.Classes in '..\src\proto\Oz.Pb.Classes.pas',
+  Oz.Pb.StrBuffer in '..\src\proto\Oz.Pb.StrBuffer.pas',
+  Oz.SGL.Collections in '..\..\Oz-SGL\src\Oz.SGL.Collections.pas',
+  Oz.SGL.HandleManager in '..\..\Oz-SGL\src\Oz.SGL.HandleManager.pas',
+  Oz.SGL.Heap in '..\..\Oz-SGL\src\Oz.SGL.Heap.pas',
+  Person in '..\data\Person.pas';
+
+{$T+}
+
+{$R *.RES}
+
+// save data to proto file
+procedure SaveData;
+var
+  Oz: TPerson;
+  Saver: TpbSaver;
+  Phone: TPhoneNumber;
+begin
+  Oz.Init;
+  try
+    // my data
+    Oz.Name := 'Marat Shaimardanov';
+    Oz.Id := 1;
+    Oz.Email := 'marat.sh.1961@gmail.com';
+
+    // single message
+    Phone.Init;
+    Phone.Number := 'qwerty';
+    Oz.MyPhone := Phone;
+
+    // my home phones
+    Phone.Init;
+    Phone.&Type := TPhoneType.HOME;
+    Phone.Number := '+7 382 224 3699';
+    Oz.Phones.Add(@Phone);
+
+    // my mobile phone
+    Phone.Init;
+    Phone.&Type := TPhoneType.MOBILE;
+    Phone.Number := '+7 913 826 2144';
+    Oz.Phones.Add(@Phone);
+
+    // save data
+    Saver.Init;
+    try
+      Saver.SavePerson(@Oz);
+      Saver.Pb.SaveToFile('person.pb');
+    finally
+      Saver.Free;
+    end;
+  finally
+    Oz.Free;
+  end;
+end;
+
+procedure ReadDataAndDump;
+var
+  Oz: TPerson;
+  PhoneNumber: PPhoneNumber;
+  Loader: TpbLoader;
+begin
+  Loader.Init;
+  try
+    Loader.Pb.LoadFromFile('person.pb');
+    Oz.Init;
+    try
+      Loader.LoadPerson(@Oz);
+      // write to console
+      Writeln('Name   : ', Oz.Name);
+      Writeln('Id     : ', IntToStr(Oz.Id));
+      Writeln('e-mail : ', Oz.Email);
+      Writeln('phone  : ', Oz.MyPhone.Number);
+      for PhoneNumber in Oz.Phones do
+      begin
+        Writeln('  Number: ', PhoneNumber.Number);
+        Writeln('  Type: ', GetEnumName(TypeInfo(TPhoneType),
+          Integer(PhoneNumber.&Type)));
+      end;
+      Readln;
+    finally
+      Oz.Free;
+    end;
+  finally
+    Loader.Free;
+  end;
+end;
+
+procedure TestPerson;
+begin
+  SaveData;
+  ReadDataAndDump;
+end;
+
+begin
+  Writeln('Run Protocol Buffer Tests');
+//  TestAll;
+  TestPerson;
+end.
