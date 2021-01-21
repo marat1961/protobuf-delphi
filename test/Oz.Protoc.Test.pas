eb237ee7b3de46bfed3ba6b3e3e06fd6c81c9121
marat1961 <marat.sh.1961@gmail.com>
2021-01-21 08:47:57 +0700

    Added metadata storage for saving and loading using the protocol buffer

--- test/Oz.Protoc.Test.pas
+++ test/Oz.Protoc.Test.pas
@@ -3,8 +3,10 @@
 interface
 
 uses
-  System.SysUtils, System.Math, TestFramework,
+  System.SysUtils, System.Math, System.TypInfo, TestFramework,
   Oz.Pb.StrBuffer, Oz.Pb.Classes, Oz.SGL.Collections;
+
+{$T+}
 
 {$Region 'TPhoneNumber'}
 
@@ -66,6 +68,21 @@ type
     procedure Free;
     // properties
     property Peoples: TsgRecordList<TPerson> read FPeoples;
+  end;
+
+{$EndRegion}
+
+{$Region 'TMetaRegister: Meta for save & load data'}
+
+  TMetaRegister = record
+    // Generate data
+    procedure GenData(var AddressBook: TAddressBook);
+    // Dump data
+    procedure Dump(var AddressBook: TAddressBook);
+    // Save data to protocol buffer
+    procedure SaveTo(const S: TpbSaver; var AddressBook: TAddressBook);
+    // Load data from protocol buffer
+    procedure LoadFrom(const L: TpbLoader; var obj);
   end;
 
 {$EndRegion}
@@ -135,6 +152,89 @@ end;
 procedure TAddressBook.Free;
 begin
   FPeoples.Free;
+end;
+
+{$EndRegion}
+
+{$Region 'TMetaRegister'}
+
+procedure TMetaRegister.GenData(var AddressBook: TAddressBook);
+var
+  Person: TPerson;
+  Phone: TPhoneNumber;
+begin
+  // data
+  Person.Init;
+  Person.Name := 'Oz Grock';
+  Person.Id := 1;
+  Person.Email := 'oz@mail.com';
+
+  // home phones
+  Phone.Init;
+  Phone.&Type := TPhoneType.HOME;
+  Phone.Number := '+7 382 224 99999';
+  Person.Phones.Add(@Phone);
+
+  Phone.Init;
+  Phone.&Type := TPhoneType.MOBILE;
+  Phone.Number := '999999';
+  Person.Phones.Add(@Phone);
+  AddressBook.Peoples.Add(@Person);
+
+  // data
+  Person.Init;
+  Person.Name := 'Marat Shaimardanov';
+  Person.Id := 2;
+  Person.Email := 'marat.sh.1961@gmail.com';
+
+  // single message
+  Phone.Init;
+  Phone.Number := 'qwerty';
+  Person.MyPhone := Phone;
+
+  // home phones
+  Phone.Init;
+  Phone.&Type := TPhoneType.HOME;
+  Phone.Number := '+7 382 224 3699';
+
+  // mobile phone
+  Phone.Init;
+  Phone.&Type := TPhoneType.MOBILE;
+  Phone.Number := '+7 913 826 2144';
+  Person.Phones.Add(@Phone);
+  AddressBook.Peoples.Add(@Person);
+end;
+
+procedure TMetaRegister.Dump(var AddressBook: TAddressBook);
+var
+  Person: PPerson;
+  PhoneNumber: PPhoneNumber;
+  i: Integer;
+begin
+  for i := 0 to AddressBook.Peoples.Count - 1 do
+  begin
+    Person := AddressBook.Peoples.Items[i];
+    // write to console
+    Writeln('Name   : ', Person.Name);
+    Writeln('Id     : ', IntToStr(Person.Id));
+    Writeln('e-mail : ', Person.Email);
+    Writeln('phone  : ', Person.MyPhone.Number);
+    for PhoneNumber in Person.Phones do
+    begin
+      Writeln('  Number: ', PhoneNumber.Number);
+      Writeln('  Type: ', GetEnumName(TypeInfo(TPhoneType),
+        Integer(PhoneNumber.&Type)));
+    end;
+  end;
+  Readln;
+end;
+
+procedure TMetaRegister.SaveTo(const S: TpbSaver; var AddressBook: TAddressBook);
+begin
+end;
+
+procedure TMetaRegister.LoadFrom(const L: TpbLoader; var obj);
+begin
 end;
 
 {$EndRegion}
